5a5af878f430a31caa9da969b4433f3d9826a734
Qiyuan Chen <qiyuan.chen62@myhunter.cuny.edu>
2016-05-04 14:43:27 -0400

    login

--- app/src/main/java/com/android/flamingo/litnyc/Activities/LoginActivity.java
+++ app/src/main/java/com/android/flamingo/litnyc/Activities/LoginActivity.java
@@ -373,8 +373,48 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         }
     }
     public void sign_up(View v){
-        UserSignUpTask task=new UserSignUpTask(mEmailView)
-        task.execute()
+        // Reset errors.
+        mEmailView.setError(null);
+        mPasswordView.setError(null);
+
+        // Store values at the time of the login attempt.
+        String email = mEmailView.getText().toString();
+        String password = mPasswordView.getText().toString();
+
+        boolean cancel = false;
+        View focusView = null;
+
+        // Check for a valid password, if the user entered one.
+        if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
+            mPasswordView.setError(getString(R.string.error_invalid_password));
+            focusView = mPasswordView;
+            cancel = true;
+        }
+
+        // Check for a valid email address.
+        if (TextUtils.isEmpty(email)) {
+            mEmailView.setError(getString(R.string.error_field_required));
+            focusView = mEmailView;
+            cancel = true;
+        } else if (!isEmailValid(email)) {
+            mEmailView.setError(getString(R.string.error_invalid_email));
+            focusView = mEmailView;
+            cancel = true;
+        }
+
+        if (cancel) {
+            // There was an error; don't attempt login and focus the first
+            // form field with an error.
+            focusView.requestFocus();
+        } else {
+            // Show a progress spinner, and kick off a background task to
+            // perform the user login attempt.
+            showProgress(true);
+            mAuthTask = new UserLoginTask(email, password);
+            mAuthTask.execute((Void) null);
+        }
+        UserSignUpTask task=new UserSignUpTask(email,password);
+        task.execute();
     }
     public class UserSignUpTask extends AsyncTask<Void, Void, Boolean> {
 
@@ -390,14 +430,12 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         protected Boolean doInBackground(Void... params) {
             // TODO: attempt authentication against a network service.
 
-            if(mEmail.equals("")||mPassword.equals("")){
-                return false;
-            }
+
             login_request request = new login_request(mEmail,mPassword);
             try {
                 login_response response = NetworkHelper.makeRequestAdapter(getApplicationContext())
                         .create(EndPoints.class).create(request);
-                Log.d("result",response.result);
+                Log.d("result", response.result);
             }catch(Exception e){
                 e.printStackTrace(System.out);
                 return false;
@@ -419,15 +457,6 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
                 startMapsActivity();
 
-            } else {
-                if(mEmail.equals("")){
-                    mEmailView.setError(getString(R.string.error_field_required));
-                    mPasswordView.requestFocus();
-                }
-                else {
-                    mPasswordView.setError(getString(R.string.error_field_required));
-                    mPasswordView.requestFocus();
-                }
             }
         }
 
